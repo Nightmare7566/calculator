3a3e84cd36e9dd7790424f64a1b08159c4e52fd2
NightmareVlad1 <68153074+nightmarevlad1@users.noreply.github.com>
2020-08-09 02:08:44 +0300

    calculator completed

---
+++ src/Ecpression.java
@@ -0,0 +1,33 @@
+class Expression  {
+    private  Integer iRez;
+    private  Main.Action action;
+
+
+
+    public  int calc(int ival, Main.Action operation){
+        if (iRez==null){
+            iRez=ival;
+        } else {    //если уже было , делаем ранее запомненное действие, а потом запоминаем новое действие
+            switch (action) {
+                case Plus:
+                    iRez +=ival;
+                    break;
+                case Minus:
+                    iRez -=ival;
+                    break;
+                case Mult:
+                    iRez = iRez*ival;
+                    break;
+                case Div:
+                    iRez = iRez/ival;
+            }
+
+        }
+        action=operation;
+        return iRez;
+    }
+    public  Integer getiRez() {
+        return iRez;
+    }
+}
+
