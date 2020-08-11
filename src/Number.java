62858e471234707d18cdefd1bdda48635b8cb239
NightmareVlad1 <mr.android.2000@mail.ru>
2020-08-11 12:58:46 +0300

    calculator completed v2.0

---
+++ src/Number.java
@@ -0,0 +1,13 @@
+public abstract class Number {
+
+
+    public abstract void sum();
+
+    public abstract void sub();
+
+    public abstract void div();
+
+    public abstract void mul();
+
+    public abstract int getResult();
+    public abstract String getStringResult();}
